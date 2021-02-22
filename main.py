ec94060f1a894041bcbcdda804097e1c1bd005a9
Yukki Chapardeur <randomimus.beakuzz@gmail.com>
2021-02-22 10:50:05 +0100

    Drew the 5 jars, a key and a snake

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 import pygame
 import os
-from functions import *
 
 
 __author__ = 'Eric-Nicolas'
@@ -17,18 +16,26 @@ IMG_WIDTH, IMG_HEIGHT = 128, 128
 JAR_IMG: pygame.Surface = pygame.transform.scale(
     pygame.image.load(os.path.join('assets', 'jar.png')), (IMG_WIDTH, IMG_HEIGHT))
 
-jar_rect = JAR_IMG.get_rect(
-    center=(
-        WIN_WIDTH // 5 - JAR_IMG.get_width() // 5,
-        WIN_HEIGHT // 2 - JAR_IMG.get_height() // 2
-    )
-)
 
 KEY_IMG: pygame.Surface = pygame.transform.scale(
     pygame.image.load(os.path.join('assets', 'key.png')), (IMG_WIDTH // 2, IMG_HEIGHT // 2))
 
+key_rect = KEY_IMG.get_rect(
+    center=(
+        WIN_WIDTH // 5 - KEY_IMG.get_width() // 5 - 10,
+        WIN_HEIGHT // 2 + 6
+    )
+)
+
 SNAKE_IMG: pygame.Surface = pygame.transform.scale(
     pygame.image.load(os.path.join('assets', 'snake.png')), (IMG_WIDTH // 2, IMG_HEIGHT // 2))
+
+snake_rect = SNAKE_IMG.get_rect(
+    center=(
+        WIN_WIDTH // 5 - JAR_IMG.get_width() // 5 + JAR_IMG.get_width() * 4,
+        WIN_HEIGHT // 2
+    )
+)
 
 FIRST_PART_COLOR = (128, 64, 64)
 SECOND_PART_COLOR = (128, 0, 0)
@@ -41,6 +48,16 @@ NUMBER_OF_PARTS = 4
 NUMBER_OF_ROUNDS = 3
 KEY = 'key'
 SNAKE = 'snake'
+
+
+def draw_jar(index: int) -> None:
+    jar_rect = JAR_IMG.get_rect(
+        center=(
+            WIN_WIDTH // 5 - JAR_IMG.get_width() // 5 + JAR_IMG.get_width() * index,
+            WIN_HEIGHT // 2
+        )
+    )
+    WIN.blit(JAR_IMG, jar_rect)
 
 
 def main() -> None:
@@ -58,9 +75,11 @@ def main() -> None:
 
         WIN.fill(ALL_COLORS[color_counter])
 
-        WIN.blit(JAR_IMG, jar_rect)
-        WIN.blit(KEY_IMG, (0, 0))
-        WIN.blit(SNAKE_IMG, (WIN_WIDTH - SNAKE_IMG.get_width(), 0))
+        for i in range(NUMBER_OF_JARS):
+            draw_jar(i)
+
+        WIN.blit(KEY_IMG, key_rect)
+        WIN.blit(SNAKE_IMG, snake_rect)
 
         pygame.display.update()
 
