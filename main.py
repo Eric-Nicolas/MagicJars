e1eb7f6186574af5a453a220c2b344e9e84969fe
Yukki Chapardeur <randomimus.beakuzz@gmail.com>
2021-02-22 12:28:59 +0100

    Added some text

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ WIN_WIDTH, WIN_HEIGHT = 800, 600
 WIN: pygame.Surface = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 pygame.display.set_caption("MagicJars")
 
+FPS = 60
+CLOCK = pygame.time.Clock()
+
 IMG_WIDTH, IMG_HEIGHT = 128, 128
 
 JAR_IMG: pygame.Surface = pygame.transform.scale(
@@ -23,18 +26,12 @@ KEY_IMG: pygame.Surface = pygame.transform.scale(
 SNAKE_IMG: pygame.Surface = pygame.transform.scale(
     pygame.image.load(os.path.join('assets', 'snake.png')), (IMG_WIDTH // 2, IMG_HEIGHT // 2))
 
-snake_rect = SNAKE_IMG.get_rect(
-    center=(
-        WIN_WIDTH // 5 - JAR_IMG.get_width() // 5 + JAR_IMG.get_width() * 4,
-        WIN_HEIGHT // 2
-    )
-)
-
+WHITE = (255, 255, 255)
 FIRST_PART_COLOR = (128, 64, 64)
 SECOND_PART_COLOR = (128, 0, 0)
 THIRD_PART_COLOR = (64, 0, 0)
 FOURTH_PART_COLOR = (64, 0, 64)
-ALL_COLORS = [FIRST_PART_COLOR, SECOND_PART_COLOR, THIRD_PART_COLOR, FOURTH_PART_COLOR]
+ALL_PART_COLORS = [FIRST_PART_COLOR, SECOND_PART_COLOR, THIRD_PART_COLOR, FOURTH_PART_COLOR]
 
 NUMBER_OF_JARS = 5
 NUMBER_OF_PARTS = 4
@@ -54,13 +51,13 @@ def draw_key(index: int) -> None:
 
 
 def draw_snake(index: int) -> None:
-    rect = SNAKE_IMG.get_rect(
+    snake_rect = SNAKE_IMG.get_rect(
         center=(
             WIN_WIDTH // 5 - JAR_IMG.get_width() // 5 + JAR_IMG.get_width() * index,
             WIN_HEIGHT // 2
         )
     )
-    WIN.blit(SNAKE_IMG, rect)
+    WIN.blit(SNAKE_IMG, snake_rect)
 
 
 def draw_jar(index: int) -> None:
@@ -79,16 +76,26 @@ def main() -> None:
     current_round = 1
     color_counter = 0
 
+    FONT = pygame.font.Font(None, 36)
+
     jars = custom_array(NUMBER_OF_JARS, KEY, SNAKE, current_part)
 
     is_running = True
     while is_running:
+        lives_label = FONT.render("Lives: " + str(lives), True, WHITE)
+        part_label = FONT.render("Part: " + str(current_part), True, WHITE)
+        round_label = FONT.render("Round: " + str(current_round), True, WHITE)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
 
-        WIN.fill(ALL_COLORS[color_counter])
+        WIN.fill(ALL_PART_COLORS[color_counter])
+
+        WIN.blit(lives_label, (10, 10))
+        WIN.blit(part_label, (WIN_WIDTH - part_label.get_width() - 39, 10))
+        WIN.blit(round_label, (WIN_WIDTH - round_label.get_width() - 10, round_label.get_height() + 10))
 
         for i in range(NUMBER_OF_JARS):
             draw_jar(i)
@@ -100,6 +107,7 @@ def main() -> None:
                 draw_snake(i)
 
         pygame.display.update()
+        CLOCK.tick(FPS)
 
 
 if __name__ == '__main__':
